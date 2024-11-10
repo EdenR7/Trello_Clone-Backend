982ccd05a45d3cabeeda343132bc743e3834e755
EdenR7 <edenrot777@gmail.com>
2024-11-10 19:10:11 +0200

    eden

--- src/app.ts
+++ src/app.ts
@@ -19,15 +19,12 @@ export async function main() {
   app.use(express.static("public"));
   const path = require("path");
   app.use(express.json());
-  // app.use(cors());
-  app.use(
-    cors({
-      origin: "*",
-      // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-      // allowedHeaders: ['Content-Type', 'Authorization'],
-      // credentials: true
-    })
-  );
+  app.use(cors());
+  // app.use(
+  //   cors({
+  //     origin: "*",
+  //   })
+  // );
 
   // Routes
   app.use("/api/auth", authRouter);
